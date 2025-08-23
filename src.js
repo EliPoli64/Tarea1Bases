1013a7b8ab8253c475652206feb4dccc434c358c
EliPoli64 <eliasramirezhernandez1@gmail.com>
2025-08-23 13:01:47 -0600

    elementos básicos UI

---
+++ src.js
@@ -0,0 +1,12 @@
+function buscarEmpleado() {
+    let filtro = document.getElementById('name').value.toLowerCase();
+    
+}
+
+function cargarInsertarEmpleado() {
+    window.location.href = 'insertar.html';
+}
+
+function volverPagPrincipal() {
+    window.location.href = 'index.html';
+}
